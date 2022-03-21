225e2ebe95b0bfafb0f00dd2dc534cec59a4dbcd
Carlos Andres <legomarth@hotmail.com>
2022-03-21 11:54:12 -0500

    feature: create welcome screen

---
+++ onboarding/onboarding_presentation/src/main/java/com/karlosprojects/onboarding_presentation/components/ActionButton.kt
@@ -0,0 +1,34 @@
+package com.karlosprojects.onboarding_presentation.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.karlosprojects.core_ui.LocalSpacing
+
+@Composable
+fun ActionButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    isEnabled: Boolean = true,
+    textStyle: androidx.compose.ui.text.TextStyle = MaterialTheme.typography.button
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = isEnabled,
+        shape = RoundedCornerShape(100.dp)
+    ) {
+        Text(
+            text = text,
+            style = textStyle,
+            color = MaterialTheme.colors.onPrimary,
+            modifier = Modifier.padding(LocalSpacing.current.spaceSmall)
+        )
+    }
+}
