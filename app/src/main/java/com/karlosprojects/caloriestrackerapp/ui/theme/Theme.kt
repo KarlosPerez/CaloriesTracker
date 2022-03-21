a9291957863ba69c0b48d42336232f71ef6b57ef
Carlos Andres <legomarth@hotmail.com>
2022-03-21 11:52:56 -0500

    feature: create core-ui module to use strings in onboarding presentation

--- app/src/main/java/com/karlosprojects/caloriestrackerapp/ui/theme/Theme.kt
+++ app/src/main/java/com/karlosprojects/caloriestrackerapp/ui/theme/Theme.kt
@@ -5,7 +5,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
+import com.karlosprojects.core_ui.Dimensions
+import com.karlosprojects.core_ui.LocalSpacing
 
 
 private val DarkColorPalette = darkColors(
@@ -43,11 +46,12 @@ fun CaloriesTrackerAppTheme(
     } else {
         LightColorPalette
     }
-
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+    CompositionLocalProvider(LocalSpacing provides Dimensions()) {
+        MaterialTheme(
+            colors = colors,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+    }
 }
