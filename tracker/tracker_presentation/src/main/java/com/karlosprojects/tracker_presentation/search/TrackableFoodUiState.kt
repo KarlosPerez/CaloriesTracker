2693e9421e50c9a76df3b845e0a86e809d700947
Carlos Andres <legomarth@hotmail.com>
2022-06-27 19:47:45 -0500

    feature: create viewModel for search feature, and events

---
+++ tracker/tracker_presentation/src/main/java/com/karlosprojects/tracker_presentation/search/TrackableFoodUiState.kt
@@ -0,0 +1,9 @@
+package com.karlosprojects.tracker_presentation.search
+
+import com.karlosprojects.tracker_domain.model.TrackableFood
+
+data class TrackableFoodUiState(
+    val food: TrackableFood,
+    val isExpanded: Boolean = false,
+    val amount: String = ""
+)
