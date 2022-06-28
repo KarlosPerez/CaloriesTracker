f52e5d0c46c3bfee7cda44c6070f201a04df3bb7
Carlos Andres <legomarth@hotmail.com>
2022-06-27 22:05:22 -0500

    fix: remove unused import

--- tracker/tracker_presentation/src/main/java/com/karlosprojects/tracker_presentation/search/components/TrackableFoodItem.kt
+++ tracker/tracker_presentation/src/main/java/com/karlosprojects/tracker_presentation/search/components/TrackableFoodItem.kt
@@ -17,7 +17,6 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
